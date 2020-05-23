9e1f5c5dfaeb172c4436cc2b147249439a89ce1f
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-23 22:40:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,10 +3,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  
+  hash[key]
 end
 
-def update_counting_hash(hash, key)
   def update_counting_hash(hash, key)
   if hash[key]
     hash[key] = hash[key] + 1
